de6e664fcf8105ce41de10b4ad6bae0508965c69
Parthiv Mangukiya <parthivm20@gmail.com>
2022-09-13 13:32:50 -0700

    Added linkedin github

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,13 @@
 function refreshQuote() {
     // var host = "http://127.0.0.1:3000"
     var host = "https://parthiv-mangukiya-website-backend.vercel.app"
+    $("#refresh").disabled = true
+    $("#refresh").removeClass("fa-refresh")
+    $("#refresh").addClass("fa-spinner")
     $.get(`${host}/quote`, data => {
+        $("#refresh").removeClass("fa-spinner")
+        $("#refresh").addClass("fa-refresh")
         $("#quoteSpan").html(data)
+        $("#refresh").disabled = false
     });
 }
